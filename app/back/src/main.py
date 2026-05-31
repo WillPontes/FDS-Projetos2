c55d1228c3fed1a17f2e1f6610ffabee07da2b5d
Igor <IgrPhillipe@gmail.com>
2026-05-31 16:32:41 -0300

    fix: update CORS middleware to allow additional origins for API access

--- app/back/src/main.py
+++ app/back/src/main.py
@@ -26,7 +26,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://taggy-ecoscore-git-feat-modules-igrphillipes-projects.vercel.app",
+                   "https://taggy-ecoscore.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
